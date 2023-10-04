745d6baa2078ab740ba11ab61003449f4d2b5906
mateusdata <mateuspele2015@gmail.com>
2023-10-04 08:25:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-gradient-to-b from-pink-400 via-pink-600 to-orange-500 min-h-screen flex flex-col justify-center items-center text-white">
+      <h1 className="text-4xl font-bold mb-4">Gestão de Horários Acadêmicos</h1>
+      <p className="text-lg">Bem-vindo ao nosso site de Engenharia de Software</p>
+    </div>
+  );
+}
+
+export default App;
