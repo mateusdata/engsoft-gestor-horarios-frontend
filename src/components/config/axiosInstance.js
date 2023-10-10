98529b62a87e888670580d86fdd49377b2e6925d
mateusdata <mateuspele2015@gmail.com>
2023-10-09 22:29:38 -0300

    update api

--- src/components/config/axiosInstance.js
+++ src/components/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://3338-2804-7d74-e3-9900-e53f-65e6-9294-688f.ngrok-free.app/',
+  baseURL: 'http://localhost:3001',
 });
 
 axiosInstance.interceptors.request.use((config) => {
