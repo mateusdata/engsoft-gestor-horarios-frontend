4d78ae1260259705169df018b8a8060e668b0fa3
mateusdata <mateuspele2015@gmail.com>
2023-10-11 06:39:03 -0300

    update baseURL

--- src/components/config/axiosInstance.js
+++ src/components/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://2b4a-2804-7d74-e3-9900-e53f-65e6-9294-688f.ngrok-free.app',
+  baseURL: 'https://apigestaohorarios.onrender.com',
 });
 
 axiosInstance.interceptors.request.use((config) => {
