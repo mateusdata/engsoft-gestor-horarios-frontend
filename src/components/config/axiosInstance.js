a820b8af6e07c8edc39ad381be1c87961490a752
mateusdata <mateuspele2015@gmail.com>
2023-10-11 03:30:23 -0300

    update axios instance

--- src/components/config/axiosInstance.js
+++ src/components/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://apigestaohorarios.vercel.app',
 });
 
 axiosInstance.interceptors.request.use((config) => {
