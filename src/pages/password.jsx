285bcfe4ad7cbc85dc98ce5795a4be326d6e3df3
vinicius-0402 <viniciusbr2029@gmail.com>
2023-10-29 17:37:13 -0300

    Alteração Imagem

--- src/pages/password.jsx
+++ src/pages/password.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'animate.css';
 import ifba from '../images/ifba.png';
-import lamp from '../images/lamp.png';
+import lampada from '../images/lampada.png';
 import { Link } from 'react-router-dom';
 
 function Password(){
@@ -22,7 +22,7 @@ function Password(){
             </div>
         </div>
         <div className='flex-col bg-[#82CF6F] w-screen h-screen justify-center items-center tracking-[7.4px] space-x-[50px] hidden md:flex'>
-            <img src={lamp} alt="" className='mt-[70px] pb-[40px]' />
+            <img src={lampada} alt="" className='mt-[70px] pb-[40px]' />
             <h1 className='text-[30px] mb-[10px]'>Redefina o ambiente</h1>
             <h1 className='text-[30px] mt-[10px]'>Abra novas portas para novos conhecimentos.</h1>
         </div>
