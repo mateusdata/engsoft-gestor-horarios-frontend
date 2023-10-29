fb0899430b9d09187eab8a17d0bfcaddda6ea6a6
mateusdata <mateuspele2015@gmail.com>
2023-10-29 17:30:47 -0300

    merge

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -49,7 +49,7 @@ function Login() {
                   </div>
                   <div className="relative">
                     <input placeholder='Digite aqui sua senha' type="password" id="password" name="password" className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="password-error" onChange={(e) => setSenha(e.target.value)} />
-                    <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                    <div className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
                       <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                       </svg>
