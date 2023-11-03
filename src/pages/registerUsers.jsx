1b829fec1987403956502b66083b460b132e1f73
isatkm22 <isadoramarques043@gmail.com>
2023-11-03 20:13:33 -0300

    tela de cadastro em adamento

--- src/pages/registerUsers.jsx
+++ src/pages/registerUsers.jsx
@@ -6,15 +6,18 @@ function RegisterUsers(){
     const [nome,setNome] = useState();
     const [email,setEmail] = useState();
     const [senha,setSenha] = useState();
-    const [tipo,setTipo] = useState(true);
+    const [tipo,setTipo] = useState();
+    const [cargo,setCargo] = useState();
+    const [matricula,setMatricula] = useState();
+    const [departamento,setDepartamento] = useState();
 
     function CadastrarUsuario(e) {
         e.preventDefault()
         
         const obj = {
-            nome,email,senha,tipo
+            nome,email,senha,tipo,cargo,matricula,departamento
         }
-        console.log(nome,email,senha);
+        console.log(nome,email,senha,cargo,matricula,departamento);
 
         axiosInstance.post("/cadastro",obj).then((response)=>{
             console.log(response)
@@ -26,26 +29,78 @@ function RegisterUsers(){
     }
 return(
 
-
     <Layouts>
-    <form action="" className=""> 
-
-        <label htmlFor="">Nome</label> <br />
-        <input placeholder="Nome" type="text" onChange={(e)=> setNome(e.target.value)} name="Nome"/>
-        <br /> 
-        <label htmlFor="">Email</label> <br />
-        <input placeholder="Email" type="text" onChange={(e)=> setEmail(e.target.value)} name="Email"/> <br />
-        <br/>
-        <label htmlFor="">Senha</label> 
-        <input placeholder="Senha" type="password" onChange={(e)=> setSenha(e.target.value)} name="Senha"/> <br/>
-        <br/> 
-        <button onClick={CadastrarUsuario}>Salvar</button>
-        <br/>
-        <label htmlFor="">Administrador</label>
-        <input defaultChecked={tipo} type="checkbox" onClick={()=> setTipo(! tipo)} name="Administrador" />
-        
-   
-    </form>  
+    <div className="relative overflow-hidden bg-gray-100 h-screen ">  
+        <div className="m-5">
+            <img src="{Logo}" alt="Logo do IFBA" />
+        </div>
+        <div className="nx-auto max-w-screen-md py-12 px-4 sm:px6 md:max-w-screen-xl md:py-20 lg:py-32 md:px-8"> 
+            <div className="md:pr-8 md:w-1/2 xl:pr-0 xl:w-5/12">
+                <h2 className="text-2xl text-gray-600 font-bold md:text-3xl md:leading-tight lg:text-4xl lg:leading-tight dark:text-gray-200 whitespace-nowrap">
+                Olá, bem-vindo ao cadastro do professor
+                </h2>
+              <form action="" className="mt-8 "> 
+                <div className="grip gap-y-4">
+                    <div>
+                        <label htmlFor="" className="block text-sm mb-2 dark:text-white">Nome</label> 
+                        <div className="relative">
+                            <input placeholder="Nome" type="text" onChange={(e)=> setNome(e.target.value)} name="Nome" required className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"/> 
+                        </div>
+                    </div> 
+                    <div> 
+                        <div className="flex justify-between intems-center">
+                         <label htmlFor="" className="block text-sm mb-2 dark:text-white">Email</label>
+                        </div>
+                        <div className="relative"> 
+                         <input placeholder="Email" type="text" onChange={(e)=> setEmail(e.target.value)} name="Email"   required  className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"/> 
+                        </div>
+                    </div>  
+                    <div> 
+                        <div className="flex justify-between intems-center">
+                         <label htmlFor="" className="block text-sm mb-2 dark:text-white">Cargo</label>
+                        </div>
+                        <div className="relative"> 
+                         <input placeholder="Informe o cargo" type="text" onChange={(e)=> setCargo(e.target.value)} name="Cargo"   required  className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"/> 
+                        </div>
+                    </div>  
+                    <div> 
+                        <div className="flex justify-between intems-center">
+                         <label htmlFor="" className="block text-sm mb-2 dark:text-white">Matricula</label>
+                        </div>
+                        <div className="relative"> 
+                         <input placeholder="Informe a matrícula" type="text" onChange={(e)=> setMatricula(e.target.value)} name="Matricula"   required  className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"/> 
+                        </div>
+                    </div>  
+                    <div> 
+                        <div className="flex justify-between intems-center">
+                         <label htmlFor="" className="block text-sm mb-2 dark:text-white">Departamento</label>
+                        </div>
+                        <div className="relative"> 
+                         <input placeholder="Informe o departamento" type="text" onChange={(e)=> setDepartamento(e.target.value)} name="Departamento"   required  className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"/> 
+                        </div>
+                    </div>  
+                    <div>
+                        <div className="flex justify-between intems-center"> 
+                         <label htmlFor="" className="block text-sm mb-2 dark:text-white">Senha</label> 
+                        </div>
+                        <div className="relative"> 
+                         <input placeholder="Senha" type="password" onChange={(e)=> setSenha(e.target.value)} name="Senha" required className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"/>
+                        </div>
+                    </div> 
+                    <div>
+                        <div className="class items-center">
+                         <label htmlFor="" className="m-2 text-sm mb-2 dark:text-white">Administrador</label>
+                         <input defaultChecked={tipo} type="checkbox" onClick={()=> setTipo(! tipo)} name="Administrador" className=""/>    
+                        </div>       
+                    </div> 
+                    <div className="flex fle-col items-center"> 
+                     <button onClick={CadastrarUsuario} className="py-3 px-4 w-60 mt-5 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-green-500 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">Cadastrar</button>
+                    </div>
+                </div>
+             </form> 
+            </div>
+        </div> 
+    </div>
     </Layouts>
 );
 
