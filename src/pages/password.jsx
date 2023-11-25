82af72cc85859eeca177360e38556183838a52b7
artur-moraes <100148375+Artur-Moraes@users.noreply.github.com>
2023-11-25 17:59:05 -0300

    Update password layout

--- src/pages/password.jsx
+++ src/pages/password.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import 'animate.css';
-import ifba from '../images/ifba.png';
+import Sifba from '../image2/sifba.svg'
 import lampada from '../images/lampada.png';
 import { Link, useNavigate } from 'react-router-dom';
 import axiosInstance from '../components/config/axiosInstance.js';
@@ -33,7 +33,7 @@ function Password(){
     <div className='flex flex-col border min-h-screen font-normal font-[KoHo] not-italic leading-none shrink-0 md:flex-row'>
         {contextHolder}
         <div className='bg-[#EFEFEF] w-screen h-screen'>
-        <img src={ifba} alt="" className='pt-[20px] pl-[20px] pb-[55px]'/>
+        <div className='flex text justify-center'><img src={Sifba} alt="logo-SIFBA" className='pt-[20px] pl-[20px] pb-[55px]'/></div>
             <div className='flex justify-center min-h-40'>
                 <form className='flex flex-col justify-center items-center'>
                     <h1 className='text-[40px] pb-[60px]'>Redefinir sua senha</h1>
@@ -41,16 +41,12 @@ function Password(){
                         <label htmlFor="email" className='text-[20px] pb-[10px] ml-5'>Email</label>
                         <div className='w-full flex flex-col justify-center items-center'><input type="email" name="email" id="email" onChange={(e)=> setemail(e.target.value)} placeholder='Email' className='w-[370px] max-w-[90%] text-[20px] rounded-[10px] py-[10px] pl-[20px] mb-[75px]' /></div>
                     </div>
-                    <button type="submit" onClick={redirect} className='bg-[#59AD4B] rounded-[10px] w-[195px] h-[45px] text-[#FFF] text-[25px] mb-[35px]'>Redefinir Senha</button>
-                    <span className='text-[20px]'>Lembrou da sua senha ? <Link to={'/login'} className='text-[#178E04]'>Faça login aqui</Link></span>
+                    <button type="submit" onClick={redirect} className='bg-green-500 rounded-[10px] w-[195px] h-[45px] text-[#FFF] text-[25px] mb-[35px]'>Redefinir Senha</button>
+                    <span className='text-[20px]'>Lembrou da sua senha ? <Link to={'/login'} className='text-green-600'>Faça login aqui</Link></span>
                 </form>
             </div>
         </div>
-        <div className='flex-col bg-[#82CF6F] w-screen h-screen justify-center items-center tracking-[7.4px] space-x-[50px] hidden md:flex'>
-            <img src={lampada} alt="" className='mt-[70px] pb-[40px]' />
-            <h1 className='text-[30px] mb-[10px]'>Redefina o ambiente</h1>
-            <h1 className='text-[30px] mt-[10px]'>Abra novas portas para novos conhecimentos.</h1>
-        </div>
+        
     </div>
   )
 }
