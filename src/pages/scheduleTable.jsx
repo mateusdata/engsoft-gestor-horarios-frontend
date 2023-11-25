157686add6633629fc396a05ab3896f87c74bbec
DanielSantosProg <danielsantos_lol@hotmail.com>
2023-11-25 10:34:15 -0300

    Fixing Schedule page resposiveness

--- src/pages/scheduleTable.jsx
+++ src/pages/scheduleTable.jsx
@@ -90,9 +90,9 @@ function ScheduleTable() {
         <Layouts>
             {/* {JSON.stringify(showSubjects)} */}
             {contextHolder}
-            <div class="flex flex-col items-center p-2 m-auto">
+            <div class="flex flex-col items-center p-2 w-full overflow-hidden m-auto">
                 <p class="text-xl px-8 relative top-4">Professores</p>
-                <div class="max-w-7xl h-18 ps-4 pe-4 py-6 text-center">
+                <div class="w-full h-18 ps-4 pe-4 py-6 text-center">
                     <Carousel>                        
                         <Button>Luan</Button>
                         <Button>Ana</Button>
@@ -104,113 +104,107 @@ function ScheduleTable() {
                         <Button>Rabelo</Button>
                     </Carousel>
                 </div>
-                <div class="px-4 py-10 sm:px-6 lg:px-20 lg:py-14 mx-auto">
-                    <div class="flex flex-col">
-                        <div class="-m-0 overflow-x-auto">
-                        <div class="p-0 min-w-full inline-block align-middle">
-                            <div class="rounded-xl shadow-sm overflow-hidden dark:bg-slate-900 dark:border-gray-700">
-                                <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                                    <thead class="bg-inherit">
-                                        <tr>
-                                            <th scope="col" class="">                                
-                                            </th>
-                                            <th scope="col" class="px-12 py-5 text-start">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
-                                                    Segunda
-                                                    </span>
-                                                </div>
-                                            </th>
+                <div class="overflow-x-auto px-4 py-2 max-w-full my-2 mx-auto">                    
+                    <div class="shadow-sm dark:bg-slate-900 dark:border-gray-700">
+                            <table class="rounded-2xl min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                                <thead class="bg-inherit">
+                                    <tr>
+                                        <th scope="col" class="">                                
+                                        </th>
+                                        <th scope="col" class="px-12 py-5 text-start">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                                                Segunda
+                                                </span>
+                                            </div>
+                                        </th>
 
-                                            <th scope="col" class="px-12 py-5 text-start">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
-                                                    Terça
-                                                    </span>
-                                                </div>
-                                            </th>
+                                        <th scope="col" class="px-12 py-5 text-start">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                                                Terça
+                                                </span>
+                                            </div>
+                                        </th>
 
-                                            <th scope="col" class="px-12 py-5 text-start">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
-                                                    Quarta
-                                                    </span>
-                                                </div>
-                                            </th>
+                                        <th scope="col" class="px-12 py-5 text-start">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                                                Quarta
+                                                </span>
+                                            </div>
+                                        </th>
 
-                                            <th scope="col" class="px-12 py-5 text-start">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
-                                                    Quinta
-                                                    </span>
-                                                </div>
-                                            </th>
+                                        <th scope="col" class="px-12 py-5 text-start">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                                                Quinta
+                                                </span>
+                                            </div>
+                                        </th>
 
-                                            <th scope="col" class="px-12 py-5 text-start">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
-                                                    Sexta
-                                                    </span>
-                                                </div>
-                                            </th>
-                                        </tr>
-                                    </thead>
+                                        <th scope="col" class="px-12 py-5 text-start">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                                                Sexta
+                                                </span>
+                                            </div>
+                                        </th>
+                                    </tr>
+                                </thead>
 
-                                    <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-                                        {array.map((item)=> (
-                                            <tr class="bg-green-200 dark:bg-slate-900 dark:hover:bg-slate-800">
-                                            <td class="h-px w-px whitespace-nowrap bg-white">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="block px-12 py-5">
-                                                        <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.hora}</span>
-                                                    </span>
-                                                </div>
-                                            </td>
-                                            <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="block px-12 py-5">
-                                                        <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
-                                                    </span>
-                                                </div>
-                                            </td>
-                                            <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="block px-12 py-5">
-                                                        <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
-                                                    </span>
-                                                </div>
-                                            </td>
-                                            <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="block px-12 py-5">
-                                                        <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
-                                                    </span>
-                                                </div>
-                                            </td>
-                                            <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="block px-12 py-5">
-                                                        <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
-                                                    </span>
-                                                </div>
-                                            </td>
-                                            <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
-                                                <div class="flex items-center justify-center gap-x-2">
-                                                    <span class="block px-12 py-5">
-                                                        <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
-                                                    </span>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        ))}                                                                                            
-                                    </tbody>
-                                </table>                    
-                            </div>
+                                <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                                    {array.map((item)=> (
+                                        <tr class="bg-green-200 dark:bg-slate-900 dark:hover:bg-slate-800">
+                                        <td class="h-px w-px whitespace-nowrap bg-white">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="block px-12 py-5">
+                                                    <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.hora}</span>
+                                                </span>
+                                            </div>
+                                        </td>
+                                        <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="block px-12 py-5">
+                                                    <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
+                                                </span>
+                                            </div>
+                                        </td>
+                                        <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="block px-12 py-5">
+                                                    <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
+                                                </span>
+                                            </div>
+                                        </td>
+                                        <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="block px-12 py-5">
+                                                    <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
+                                                </span>
+                                            </div>
+                                        </td>
+                                        <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="block px-12 py-5">
+                                                    <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
+                                                </span>
+                                            </div>
+                                        </td>
+                                        <td class="h-px w-px hover:bg-gray-50 whitespace-nowrap">
+                                            <div class="flex items-center justify-center gap-x-2">
+                                                <span class="block px-12 py-5">
+                                                    <span class="font-mono text-center text-sm text-black dark:text-blue-500">{item.nome}</span>
+                                                </span>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    ))}                                                                                            
+                                </tbody>
+                            </table>                    
                         </div>
                     </div>
-                </div>
-                </div>
-            </div> 
+                </div>                           
         </Layouts>
     );
 
