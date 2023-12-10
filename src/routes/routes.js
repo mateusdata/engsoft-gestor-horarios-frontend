9849858c8b472478fdcf6ea4f216424117337760
vinicius-0402 <viniciusbr2029@gmail.com>
2023-12-10 20:07:55 -0300

    Nova Alteração

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -43,8 +43,7 @@ const MyRoutes = () => {
         <Route exact path="*" element={<NotFound />} />
         <Route exact path="/register" element={<Private> <RegisterUsers/> </Private>} />
         <Route exact path="/teachers" element={<Private><Teachers /></Private>} />
-        <Route exact path="/horarios" element={<Private><ScheduleTable /></Private>} />
-        
+ 
 
         
       </Routes>
