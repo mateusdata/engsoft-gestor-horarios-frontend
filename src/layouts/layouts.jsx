8744da1e1538f2b420d29cf3717e172afc6e08b1
Mateus Santos <mateuspele2015@gmail.com>
2024-02-23 22:03:27 -0300

    added:new features for calendar

--- src/layouts/layouts.jsx
+++ src/layouts/layouts.jsx
@@ -145,14 +145,14 @@ function Layouts({ children }) {
               </li>
 
               <li>
-                <Link onClick={()=> {setCurrentPage(5)}} to={"/"} className={` ${currenPage === 5 && "bg-[#D9D9D9]  dark:bg-blue-700 "}  hs-accordion-toggle flex items-center gap-x-3.5  dark:text-gray-100 py-2 px-2.5 hs-accordion-active:text-white hs-accordion-active:hover:bg-transparent text-sm dard rounded-md hover:bg-[#ADD1A7] hover:text-white`}>
+                <Link onClick={()=> {setCurrentPage(5)}} to={"/calendar"} className={` ${currenPage === 5 && "bg-[#D9D9D9]  dark:bg-blue-700 "}  hs-accordion-toggle flex items-center gap-x-3.5  dark:text-gray-100 py-2 px-2.5 hs-accordion-active:text-white hs-accordion-active:hover:bg-transparent text-sm dard rounded-md hover:bg-[#ADD1A7] hover:text-white`}>
                 <svg className="w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
                   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
                 </svg>
                 Calendario
               </Link></li>
-              <li><Link onClick={()=> {setCurrentPage(6)}} to={"/"} className={` ${currenPage === 6 && "bg-[#D9D9D9]  dark:bg-blue-700 "}  hs-accordion-toggle flex items-center gap-x-3.5  dark:text-gray-100 py-2 px-2.5 hs-accordion-active:text-white hs-accordion-active:hover:bg-transparent text-sm dard rounded-md hover:bg-[#ADD1A7] hover:text-white`}>
+              <li><Link onClick={()=> {setCurrentPage(6)}} to={"https://github.com/mateusdata?tab=repositories"} className={` ${currenPage === 6 && "bg-[#D9D9D9]  dark:bg-blue-700 "}  hs-accordion-toggle flex items-center gap-x-3.5  dark:text-gray-100 py-2 px-2.5 hs-accordion-active:text-white hs-accordion-active:hover:bg-transparent text-sm dard rounded-md hover:bg-[#ADD1A7] hover:text-white`}>
                 <svg className="w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
                 </svg>
