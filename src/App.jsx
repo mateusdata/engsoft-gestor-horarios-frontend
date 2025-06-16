b797f73b541d8a125110edd48f3a1138805eece9
Mateus Santos <54743403+mateusdata@users.noreply.github.com>
2025-06-16 22:35:08 +0000

    chore: Uncomment if you have a CSS file to import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MyRoutes from './routes/routes'
-
+// import './App.css' // Uncomment if you have a CSS file to import
 const App = () => {
   return (
     <MyRoutes/>
